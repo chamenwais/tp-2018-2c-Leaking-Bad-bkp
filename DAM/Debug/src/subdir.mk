b6b5cf2599e9bdf7e5ea8ee80ef87e27b8b7cf13
Francisco Romero <francisco.a.romero.5@gmail.com>
2018-09-14 08:21:02 -0300

    Se sube archivo requerido por el makefile para que el DAM compile

---
+++ DAM/Debug/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/ConexionesEntrantes.c \
+../src/ConexionesSalientes.c \
+../src/Contexto.c \
+../src/ElDiego.c \
+../src/Utilidades.c 
+
+OBJS += \
+./src/ConexionesEntrantes.o \
+./src/ConexionesSalientes.o \
+./src/Contexto.o \
+./src/ElDiego.o \
+./src/Utilidades.o 
+
+C_DEPS += \
+./src/ConexionesEntrantes.d \
+./src/ConexionesSalientes.d \
+./src/Contexto.d \
+./src/ElDiego.d \
+./src/Utilidades.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
