9fb83ee956648e25562e6e98133ad19eed1d9562
Andres Marcovecchio <andreshmarco@gmail.com>
2018-12-18 17:49:26 -0300

    Archivo de bash

--- bash.sh
+++ bash.sh
@@ -18,4 +18,24 @@ sudo make
 git clone https://github.com/sisoputnfrba/fifa-examples.git
 cd ..
 cd ..
+cd CPU/Debug
+sudo make clean
+sudo make
+cd ..
+cd ..
+cd DAM/Debug
+sudo make clean
+sudo make
+cd ..
+cd ..
+cd SAFA/Debug
+sudo make clean
+sudo make
+cd ..
+cd ..
+cd FunesMemory9/Debug
+sudo make clean
+sudo make
+cd ..
+cd ..
 cd ..
