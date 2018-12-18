e6d3051e3734ac4e4f3f800ccbae4196bceedd67
Andres Marcovecchio <andreshmarco@gmail.com>
2018-12-18 16:30:05 -0300

    Archivo de bash

---
+++ bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ../home/utnso
+git clone https://github.com/sisoputnfrba/tp-2018-2c-Leaking-Bad.git
+cd tp-2018-2c-Leaking-Bad/COM/lqvg
+sudo make clean
+sudo make sudo make install
+cd ..
+cd ..
+cd FS/Debug
+sudo make clean
+sudo make
+git clone https://github.com/sisoputnfrba/fifa-examples.git
